2054beb4f936b25c810af322a255ce3925421105
RuzanAlaql <Ruzan.alaql@gameit.ai>
2023-02-13 00:51:20 +0300

    Player ship movement / player movement

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,28 +5,56 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
-    [SerializeField] float _Speed;
+    private Vector3 playerVelocity;
+    [SerializeField] bool groundedPlayer;
+    [SerializeField] float playerSpeed = 6;
+    [SerializeField] float jumpHeight = 1.0f;
+    [SerializeField] float gravityValue = -9.81f;
     [SerializeField] float _TurnSmooth;
     [SerializeField] float _TurnSmoothVelocity;
-    // Start is called before the first frame update
-    void Start()
-    {
 
+    private void Start()
+    {
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         float _Horizontal = Input.GetAxis("Horizontal");
         float _Vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * _Vertical + transform.right * _Horizontal;
+        controller.Move(playerSpeed*Time.deltaTime*move);
+
         Vector3 direction = new Vector3(-_Horizontal, 0f, -_Vertical).normalized;
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+
+        if (direction != Vector3.zero)
+        {
+            transform.forward = direction;
+        }
 
         if (direction.magnitude >= 0.1f)
         {
             float _TargetAngel = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float _Angel = Mathf.SmoothDampAngle(transform.eulerAngles.y, _TargetAngel, ref _TurnSmoothVelocity, _TurnSmooth);
             transform.rotation = Quaternion.Euler(0f, _Angel, 0f);
-            controller.Move(direction * _Speed);
+            controller.Move(direction * playerSpeed);
         }
+
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
--- Assets/scripts/PlayerShipController.cs
+++ Assets/scripts/PlayerShipController.cs
@@ -4,66 +4,59 @@ using UnityEngine;
 
 public class PlayerShipController : MonoBehaviour
 {
-
-   // [SerializeField] 
-
-    [SerializeField] float _moveMentSpeed;
-    [SerializeField] float _turningSpeed;
-    [SerializeField] float _decelerate; 
-    [SerializeField] float _WaitTimeToStop; 
-
-    [SerializeField]bool canRotate;
-    [SerializeField]bool isThereMotion; 
-
     Rigidbody rb;
-    private void Start()
+    private Vector3 lastPosition;
+    private Vector3 direction = Vector3.zero;
+
+    [Header("Movement")]
+    [SerializeField] float _moveMentSpeed;
+    [SerializeField] float currentSpeed;
+    [SerializeField] bool canRotate;
+
+
+    [Header("Steering")]
+    [SerializeField] float _turningSpeed;
+
+
+    public void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-        isThereMotion = true;
     }
 
     private void Update()
     {
+        float Vertical = Input.GetAxis("Vertical");
+        float Horizontal = Input.GetAxis("Horizontal");
+
+        direction = new Vector3(Horizontal, Vertical, 0).normalized;
 
         canRotate = false;
-        float _Horizontal = Input.GetAxis("Horizontal");
-        float _Vertical = Input.GetAxis("Vertical");
-
-        if (Input.GetAxis("Vertical") > 0.2f)
-        {
-            rb.AddRelativeForce(Vector3.forward * _moveMentSpeed * Time.fixedDeltaTime * Input.GetAxis("Vertical"));
-            canRotate = true;
-            if (canRotate == true)
-                if (Input.GetAxis("Horizontal") < -0.2f || Input.GetAxis("Horizontal") > 0.2f)
+       
+                if (Vertical > 0.2f)
                 {
-                    transform.rotation = Quaternion.EulerRotation(0, transform.rotation.ToEulerAngles().y + Input.GetAxis("Horizontal") * _turningSpeed * Time.fixedDeltaTime, 0);
-                    
-
-                        rb.AddTorque(0f, _Horizontal * _turningSpeed * Time.deltaTime, 0f);
-                        //rb.AddForce(transform.right * h * _turningSpeed * Time.deltaTime);
-
+                    canRotate = true;
                 }
-            rb.AddForce(transform.forward * _Vertical * _moveMentSpeed * Time.deltaTime);
-            isThereMotion = false;
-        }
-        else
-        {
-            rb.velocity = rb.velocity * _decelerate * Time.deltaTime;
-            StartCoroutine(Stopping());
-            //rb.AddForce((-transform.forward * v) /(_decelerate * Time.deltaTime));
-        }
-
-        Debug.Log("h = "+ _Horizontal + "v = "+ _Vertical);
-
+           
     }
 
-
-    IEnumerator Stopping()
+    private void FixedUpdate()
     {
-        yield return new WaitForSeconds(_WaitTimeToStop);
-        isThereMotion = true;
+
+
+        currentSpeed = (transform.position - lastPosition).magnitude / Time.deltaTime;
+
+        //Save the position for the next update
+        lastPosition = transform.position;
+        if (canRotate == true)
+         { rb.AddTorque(0, direction.x *_turningSpeed, 0); }
+
+
+        rb.AddForce(transform.forward * direction.y * _moveMentSpeed);
     }
 
-    
 }
+
+
+
+
+
